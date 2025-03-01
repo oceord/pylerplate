50ca65d000667556aa8faf716bd00d9691e07329
oceord <73301852+oceord@users.noreply.github.com>
2025-03-01 13:19:57 +0000

    Set up pre-commit hook on devcontainer creation

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -4,8 +4,12 @@ set -o nounset
 set -o pipefail
 
 sudo chown vscode:vscode .venv/
+
 sudo ./scripts/install_meta_packages.sh \
     --install-system-common-packages \
     --install-system-dev-packages
+
 ./scripts/install_meta_packages.sh --use-pipx \
     --install-pip-dev-packages
+
+cp .githooks/pre-commit .git/hooks
